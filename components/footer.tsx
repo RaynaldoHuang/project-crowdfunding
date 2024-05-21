573986318d3a16b6c92d5019ca2a72f0d371345d
RaynaldoHuang <huangraynaldo@gmail.com>
2024-05-21 19:18:56 +0700

    modify: fixed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,42 +6,54 @@ import instagram from "@/public/svgs/Instagram.svg"
 import Link from "next/link";
 
 export default function Footer() {
+    const getYear = () => {
+        const year = new Date;
+        return year.getFullYear()
+    }
+
     return (
         <div>
-            <div className="w-11/12 mx-auto pt-6 pb-6 mt-12">
-                <div>
-                    <Image src={logo} alt={""} width={120}
-                    ></Image>
-                    <p className="text-slate-500 text-base mt-6"> Sekecil apapun donasi yang Anda berikan akan sangat berarti bagi mereka, yuk berdonasi sekarang juga untuk membantu sesama manusia yang membutuhkan.</p>
-                </div>
-                <div className="flex gap-6 mt-8">
-                    <Link href={""}><Image src={facebook} alt={""} height={40}
-                    ></Image></Link>
-                    <Link href={""}><Image src={twitter} alt={""} height={40}
-                    ></Image></Link>
-                    <Link href={""}><Image src={instagram} alt={""} height={40}
-                    ></Image></Link>
-                </div>
-                <div className="flex flex-row">
-                    <div>
-                        <h1 className="text-lg font-bold text-sky-900 mt-12 mb-6 mr-32">Menu</h1>
-                        <div className="flex flex-col gap-y-2">
-                            <Link href={""} className="text-slate-500 text-base">Fitur</Link>
-                            <Link href={""} className="text-slate-500 text-base">Kampanye</Link>
-                            <Link href={""} className="text-slate-500 text-base">Sukarelawan</Link>
+            <div className="lg:max-w-8xl mx-auto">
+                <div className="w-11/12 mx-auto pt-6 pb-6 mt-12 lg:justify-between lg:flex lg:flex-row">
+                    <div className="lg:w-1/2">
+                        <Image src={logo} alt={""} width={120}
+                        ></Image>
+                        <p className="text-slate-500 text-base mt-6 text-balance lg:w-3/4">Sekecil apapun donasi yang Anda berikan akan sangat berarti bagi mereka, yuk berdonasi sekarang juga untuk membantu sesama manusia yang membutuhkan.</p>
+                        <div className="flex gap-6 mt-8">
+                            <Link href={""}><Image src={facebook} alt={""} height={40}
+                            ></Image></Link>
+                            <Link href={""}><Image src={twitter} alt={""} height={40}
+                            ></Image></Link>
+                            <Link href={""}><Image src={instagram} alt={""} height={40}
+                            ></Image></Link>
                         </div>
                     </div>
-                    <div>
-                        <h1 className="text-lg font-bold text-sky-900 mt-12 mb-6">Kategori</h1>
-                        <div className="flex flex-col gap-y-2">
-                            <Link href={""} className="text-slate-500 text-base">Uang</Link>
-                            <Link href={""} className="text-slate-500 text-base">Mainan Anak-Anak</Link>
-                            <Link href={""} className="text-slate-500 text-base">Bantuan Obat</Link>
-                            <Link href={""} className="text-slate-500 text-base">Makanan Sehat</Link>
+                    <div className="flex flex-row lg:w-1/3 lg:justify-between lg:ml-10">
+                        <div className="">
+                            <h1 className="text-lg font-bold text-sky-900 mt-12 mb-6 mr-32 lg:mt-0 lg:mr-0">Menu</h1>
+                            <div className="flex flex-col gap-y-2">
+                                <Link href={""} className="text-slate-500 text-base">Fitur</Link>
+                                <Link href={""} className="text-slate-500 text-base">Kampanye</Link>
+                                <Link href={""} className="text-slate-500 text-base">Sukarelawan</Link>
+                            </div>
+                        </div>
+                        <div className="">
+                            <h1 className="text-lg font-bold text-sky-900 mt-12 mb-6 lg:mt-0">Kategori</h1>
+                            <div className="flex flex-col gap-y-2">
+                                <Link href={""} className="text-slate-500 text-base">Uang</Link>
+                                <Link href={""} className="text-slate-500 text-base">Mainan Anak-Anak</Link>
+                                <Link href={""} className="text-slate-500 text-base">Bantuan Obat</Link>
+                                <Link href={""} className="text-slate-500 text-base">Makanan Sehat</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
+                
+                <hr className="w-11/12 mx-auto my-7 lg:max-w-8xl"/>
+
+                <p className="w-11/12 mx-auto text-center text-sm lg:max-w-8xl mb-7">&copy; {getYear()} Sedekah. All rights reserved</p>
             </div>
+
         </div>
     );
 }
--- components/main/features-v2.tsx
+++ components/main/features-v2.tsx
@@ -8,8 +8,7 @@ export default function FeaturesV2() {
             <div className="lg:max-w-8xl mx-auto lg:mt-32">
                 <div className="w-11/12 mx-auto pt-6 pb-6 lg:flex lg:flex-row lg:items-center lg:justify-between mt-20 lg:gap-x-20">
                     <div>
-                        <Image src={img3} alt={""} height={400}
-                        ></Image>
+                        <Image src={img3} alt={""} height={400} />
                     </div>
                     <div className="lg:w-1/2">
                         <h1 className="text-2xl font-bold text-sky-900 mt-4 lg:text-4xl text-balance">Kami yakin kami bisa menghemat
