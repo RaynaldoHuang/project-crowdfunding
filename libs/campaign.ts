3e79fd48d9ddd8313ad1e338df3986533cd17eef
winstencoellins <winstencoellinss@gmail.com>
2024-05-21 20:28:14 +0700

    add: api route, campaign dummy data

--- libs/campaign.ts
+++ libs/campaign.ts
@@ -1,3 +1,8 @@
 const campaigns = [
-    {}
+    {id: 0, title: "Title 1", desc: "Desc 1", amount: 200000},
+    {id: 1, title: "Title 1", desc: "Desc 1", amount: 200000},
+    {id: 2, title: "Title 1", desc: "Desc 1", amount: 200000},
+    {id: 3, title: "Title 1", desc: "Desc 1", amount: 200000},
 ]
+
+export default {campaigns}
