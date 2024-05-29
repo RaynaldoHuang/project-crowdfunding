4d580c0910706167c4a70391d05dff2b915ad268
winstencoellins <winstencoellinss@gmail.com>
2024-05-29 21:09:02 +0700

    add: db.ts file

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import Footer from "@/components/footer";
 export default function Home() {
   return (
     <NextUIProvider>
-      <NavigationBar />
+      {/* <NavigationBar /> */}
       <Hero />
       <Statistik />
       <Features />
