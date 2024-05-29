7b37668c7e556773d67e79a759a63d579730d3ea
RaynaldoHuang <huangraynaldo@gmail.com>
2024-05-29 20:01:47 +0700

    modify/add: navigation and layout/login page

---
+++ app/Login/page.tsx
@@ -0,0 +1,14 @@
+export default function LoginPage() {
+    return (
+        <>
+            <div className="flex justify-center items-center h-screen">
+                <div className="drop-shadow-md bg-white rounded-lg">
+                    <div className="flex flex-col">
+                        <h1>Selamat Datang Kembali</h1>
+                        <p>Silahkan Masukkan Email dan Password Kamu</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
