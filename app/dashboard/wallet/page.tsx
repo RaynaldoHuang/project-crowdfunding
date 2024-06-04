a3be56b4e680b4314689b456b5f73ccad7ab0fa8
WINSTEN COELLINS <62231201@wil.local>
2024-06-04 13:21:23 +0700

    add: admin campaign and member dashboard page

--- app/dashboard/wallet/page.tsx
+++ app/dashboard/wallet/page.tsx
@@ -6,10 +6,10 @@ export default function Wallet() {
         <div className="ml-64">
             <div className="flex justify-between mx-5 h-14 items-center text-2xl font-semibold">
                 <h1>Wallet</h1>
-                <Image src={placeholder} width={60} height={60} alt='placeholder' className="rounded-full"/> 
+                <Image src={placeholder} width={45} height={45} alt='placeholder' className="rounded-full bg-transparent border px-1 py-1 border-[#336DFF]"/>
             </div>
 
-            <div className='mt-5 mx-5'>
+            <div className='mt-5 mx-5 bg-white px-5 py-10 mb-10 shadow-xl rounded-xl'>
                 <h1>Wallet Content</h1>
             </div>
         </div>
