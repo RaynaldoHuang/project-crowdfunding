d99500e557e55907cee527c222b9250937584e94
RaynaldoHuang <huangraynaldo@gmail.com>
2024-07-10 01:41:06 +0700

    modify: Input Nominal

--- app/dashboard/campaign/[slug]/donate/page.jsx
+++ app/dashboard/campaign/[slug]/donate/page.jsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation"
 import { useEffect } from "react"
+import { useState } from 'react';
 
 export default function Donate() {
     useEffect(() => {
@@ -44,15 +45,43 @@ export default function Donate() {
     }
 
     return (
-        <div className="ml-64 mt-20 px-5">
-            <h1>Donasi untuk asdfaasdf</h1>
+        <>
+            <div className="ml-64">
+                <div className="mt-20 mx-5 bg-white px-5 py-5 mb-10 rounded-xl">
+                    <div className="text-lg font-bold">
+                        Masukkan Nominal Donasi
+                    </div>
+                    <div className='mt-3'>
+                        
+                            <form onSubmit={handleSubmit}>
+                                <div className="flex flex-col">
+                                    <div className="flex border border-gray-200 rounded-lg px-3 py-2 bg-gray-100">
+                                        <span className="mr-2 font-bold">Rp</span>
+                                        <input
+                                            name="amount"
+                                            type="number"
+                                            placeholder='0'
+                                            required={true}
+                                            className="flex-1 outline-none bg-gray-100 w-full [&::-webkit-inner-spin-button]:appearance-none"
+                                        />
+                                    </div>
+                                    <button type='submit' className="mt-5 bg-sky-600 text-white px-6 py-2.5 rounded-xl text-sm w-48">Donasi Sekarang</button>
+                                </div>
+                            </form>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+        // <div className="ml-64 mt-20 px-5">
+        //     <h1>Donasi untuk asdfaasdf</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label>Jumlah Donasi</label>
-                <input name='amount' type='number' className="px-4 py-2" />
+        //     <form onSubmit={handleSubmit}>
+        //         <label>Jumlah Donasi</label>
+        //         <input name='amount' type='number' className="px-4 py-2" />
 
-                <button type='submit' className="bg-red-200">Donasi</button>
-            </form>            
-        </div>
+        //         <button type='submit' className="bg-red-200">Donasi</button>
+        //     </form>            
+        // </div>
     )
 }
