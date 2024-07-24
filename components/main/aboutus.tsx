8e4cc312293d4bdcecfdb614d6daee4b7abc7212
RaynaldoHuang <huangraynaldo@gmail.com>
2024-07-25 02:53:39 +0700

    modify : landing page

--- components/main/aboutus.tsx
+++ components/main/aboutus.tsx
@@ -1,4 +1,6 @@
-import img1 from "@/public/images/img1.jpg";
+import prof1 from "@/public/images/prof1.png";
+import prof2 from "@/public/images/prof2.png";
+import prof3 from "@/public/images/prof3.png";
 import Image from "next/image";
 
 export default function AboutUs() {
@@ -8,33 +10,20 @@ export default function AboutUs() {
         <div className="w-11/12 mx-auto mt-10 pb-12 overflow-x-hidden">
           <div className="mb-8">
             <div className="lg:flex lg:flex-col lg:items-center">
-              <p className="text-orange-500 text-base mt-10">Tentang Kami</p>
+              <p className="text-orange-500 text-base mt-10">TENTANG KAMI</p>
               <h1 className="text-2xl lg:text-center font-bold text-sky-900 mt-4 lg:text-4xl lg:w-7/12">
-                Apa yang bisa Anda sumbangkan kepada mereka yang membutuhkan?
+                Kami akan selalu menjaga kepercayaan anda kepada kami
               </h1>
               <p className="text-slate-500 text-base mt-4 lg:w-1/2 lg:text-center">
-                Ada banyak hal yang mereka butuhkan dari mainan, makanan,
-                pakaian dan sebagainya. Mereka sangat senang ketika mereka
-                memiliki hal-hal yang mereka miliki.
+                Visi dan misi ini diarahkan untuk menciptakan platform crowdfunding yang tidak hanya berfokus pada mengumpulkan dana, tetapi juga pada membangun komunitas yang kuat dan berkelanjutan
               </p>
             </div>
           </div>
-          <div className="grid grid-cols-4 justify-between gap-x-8 mt-20">
-            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4">
-              {/* <Image alt="" src={img1} height={500} width={500} className="rounded-full"></Image> */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-13"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-
+          <div className="lg:grid lg:grid-cols-3 justify-between gap-x-8 mt-20">
+            <div className="border border-slate-300 rounded-xl pt-4 pb-6 flex flex-col items-center mb-4 lg:mb-0">
+              <div className="mb-5">
+                <Image alt="" src={prof1} className="rounded-full w-60 h-60"></Image>
+              </div>
               <div className="pb-0 pt-2 flex-col items-start">
                 <h1 className="text-xl font-semibold text-balance line-clamp-2 text-center">
                   Andian Bramlie, S.kom
@@ -45,77 +34,30 @@ export default function AboutUs() {
               </div>
             </div>
 
-            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4">
-              {/* <Image alt="" src={img1} height={500} width={500} className="rounded-full"></Image> */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-13"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-
+            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4 flex flex-col items-center mb-4 lg:mb-0">
+              <div className="mb-5">
+                <Image alt="" src={prof2} className="rounded-full w-60 h-60"></Image>
+              </div>
               <div className="pb-0 pt-2 flex-col items-start">
                 <h1 className="text-xl font-semibold text-balance line-clamp-2 text-center">
-                  Andian Bramlie, S.kom
+                  Yeremy Frederick
                 </h1>
                 <p className="text-slate-500 text-base mt-1 line-clamp-3 text-balance text-center">
-                  Ketua Organisasi
+                  Wakil Ketua Organisasi
                 </p>
               </div>
             </div>
 
-            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4">
-              {/* <Image alt="" src={img1} height={500} width={500} className="rounded-full"></Image> */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-13"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-
-              <div className="pb-0 pt-2 flex-col items-start">
-                <h1 className="text-xl font-semibold text-balance line-clamp-2 text-center">
-                  Andian Bramlie, S.kom
-                </h1>
-                <p className="text-slate-500 text-base mt-1 line-clamp-3 text-balance text-center">
-                  Ketua Organisasi
-                </p>
+            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4 flex flex-col items-center">
+              <div className="mb-5">
+                <Image alt="" src={prof3} className="rounded-full w-60 h-60"></Image>
               </div>
-            </div>
-
-            <div className="border border-slate-300 rounded-xl pt-4 pb-6 px-4">
-              {/* <Image alt="" src={img1} height={500} width={500} className="rounded-full"></Image> */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-13"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-
               <div className="pb-0 pt-2 flex-col items-start">
                 <h1 className="text-xl font-semibold text-balance line-clamp-2 text-center">
-                  Andian Bramlie, S.kom
+                  Raynaldo
                 </h1>
                 <p className="text-slate-500 text-base mt-1 line-clamp-3 text-balance text-center">
-                  Ketua Organisasi
+                  Bendahara
                 </p>
               </div>
             </div>
