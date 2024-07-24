5adc93aa1cbce79f4fac520f4d1cb82b46e262fd
winstencoellins <winstencoellins13@gmail.com>
2024-07-25 00:02:47 +0700

    reset: next/router to next/navigation

--- app/dashboard/form-pengajuan/form/page.tsx
+++ app/dashboard/form-pengajuan/form/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 
 export default function ajukanForm() {
